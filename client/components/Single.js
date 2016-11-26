b5d112623d31445213348fc2a202e57ef0d8705d
Aayush Shah <shahaayush@outlook.com>
2016-11-26 18:37:11 +0530

    working react-router

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Single = React.createClass({
+	render(){
+		return(
+			<div className="single-photo">
+			I am in single-photo
+			</div>
+			)
+	}
+
+});
+
+export default Single;
